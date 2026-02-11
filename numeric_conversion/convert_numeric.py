019683053b33ca457e81eb634dfc23ec0115cc4f
Berna-96 <matteobernardini8796@gmail.com>
2026-02-11 14:17:02 +0100

    convert_numeric.py

--- numeric_conversion/convert_numeric.py
+++ numeric_conversion/convert_numeric.py
@@ -1,1 +1,7 @@
+import pandas as pd
+
+def convert_numeric(df):
+    df["age"] = pd.to_numeric(df["age"], errors="coerce")
+    df["salary"] = pd.to_numeric(df["salary"], errors="coerce")
+    return df
 
