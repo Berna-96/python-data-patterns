c84ab7ce2e8e2b6c26fcd4d88633e2b7c5378611
Berna-96 <matteobernardini8796@gmail.com>
2026-02-11 14:19:11 +0100

    validate_age.py

--- validation_rules/validate_age.py
+++ validation_rules/validate_age.py
@@ -1,1 +1,11 @@
+import pandas as pd
+
+def validate_age(df, config):
+    min_age = config["age"]["min"]
+    max_age = config["age"]["max"]
+
+    invalid_mask = (df["age"] < min_age) | (df["age"] > max_age)
+
+    df.loc[invalid_mask, "age"] = pd.NA
+    return df
 
