3c2e057a3f845414bdc570bde6fa990ff54e857d
Berna-96 <matteobernardini8796@gmail.com>
2026-02-11 14:19:42 +0100

    validate_department.py

--- validation_rules/validate_department.py
+++ validation_rules/validate_department.py
@@ -1,1 +1,10 @@
+import pandas as pd
+
+def validate_department(df, config):
+    allowed = config["department"]["allowed"]
+
+    invalid_mask = ~df["department"].str.lower().isin(allowed)
+
+    df.loc[invalid_mask, "department"] = pd.NA
+    return df
 
