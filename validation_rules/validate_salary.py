126fd5e6dc9fa76c0e1fa8cfc7b8e0a8c7dd59ba
Berna-96 <matteobernardini8796@gmail.com>
2026-02-11 14:19:27 +0100

    validate_salary.py

--- validation_rules/validate_salary.py
+++ validation_rules/validate_salary.py
@@ -1,1 +1,11 @@
+import pandas as pd
+
+def validate_salary(df, config):
+    min_salary = config["salary"]["min"]
+    max_salary = config["salary"]["max"]
+
+    invalid_mask = (df["salary"] < min_salary) | (df["salary"] > max_salary)
+
+    df.loc[invalid_mask, "salary"] = pd.NA
+    return df
 
