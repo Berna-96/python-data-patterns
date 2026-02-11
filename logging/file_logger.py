25e6960a278fdf7ffee0dcc8f670c47b8c53982f
Berna-96 <matteobernardini8796@gmail.com>
2026-02-11 14:17:30 +0100

    file_logger.py

--- logging/file_logger.py
+++ logging/file_logger.py
@@ -1,1 +1,8 @@
+from datetime import datetime
+
+def write_log(level, message):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    with open("log.txt", "a", encoding="utf-8") as f:
+        f.write(f"[{timestamp}] [{level}] {message}\n")
 
