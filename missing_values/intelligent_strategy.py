74b639ae878df7c0c12876060634381817e604f5
Berna-96 <matteobernardini8796@gmail.com>
2026-02-12 12:45:12 +0100

    intelligent_strategy.py

--- missing_values/intelligent_strategy.py
+++ missing_values/intelligent_strategy.py
@@ -1,20 +1,21 @@
 import pandas as pd
+from logging.file_logger import write_log
 
 def handle_missing_intelligent(df):
     for column in df.columns:
 
-        # NUMERIC COLUMNS
         if pd.api.types.is_numeric_dtype(df[column]):
             skewness = df[column].skew()
 
-            # Heuristic: if distribution is skewed, use median
             if abs(skewness) > 1:
+                write_log("INFO", f"Column {column} → skewed distribution → median applied")
                 df[column] = df[column].fillna(df[column].median())
             else:
+                write_log("INFO", f"Column {column} → symmetric distribution → mean applied")
                 df[column] = df[column].fillna(df[column].mean())
 
-        # CATEGORICAL COLUMNS
         else:
+            write_log("INFO", f"Column {column} → categorical → filled with 'unknown'")
             df[column] = df[column].fillna("unknown")
 
     return df
